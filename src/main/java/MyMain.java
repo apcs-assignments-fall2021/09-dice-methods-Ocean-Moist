bc294dac96cb1a32dc0b5cd7089f8224da1e5aa5
m1cr050f75ux5 <rohanzealous@gmail.com>
2021-09-28 19:24:08 -0700

    doneroni pizza

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,8 +2,7 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        return (int) (Math.random()*6 + 1);
     }
 
     // Calculate the probability of rolling at least one 6 when rolling 
@@ -15,25 +14,57 @@ public class MyMain {
 
         // Your code should roll 6 dice 10,000 times, so you should have a
         // for loop such as:
+        int x = 0;
         for (int i = 0; i < 10000; i++) {
-
+            for (int j = 0; j < 6; j++) {
+                if (rollDie() == 6) {
+                    x += 1;
+                    break;
+                }
+            }
         }
 
-        return -1.0;
+        return x/100;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int y = 0;
+        int x = 0;
+        for (int i = 0; i < 10000; i++) {
+            for (int j = 0; j < 12; j++) {
+                if (rollDie() == 6) {
+                    y += 1;
+                }
+            }
+            if (y >= 2) {
+                x += 1;
+            }
+            y = 0;
+        }
+
+        return x/100;
     }
 
     // Calculate the probability of rolling at least three 6's when rolling
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int y = 0;
+        int x = 0;
+        for (int i = 0; i < 10000; i++) {
+            for (int j = 0; j < 18; j++) {
+                if (rollDie() == 6) {
+                    y += 1;
+                }
+            }
+            if (y >= 3) {
+                x += 1;
+            }
+            y = 0;
+        }
+
+        return x/100;
     }
 
     public static void main(String[] args) {
